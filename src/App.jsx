8102297082c20457a48726325a7d17d220c703c0
JustVibhor <vibhorebhatneriya@gmail.com>
2023-08-31 15:52:17 +0530

    To avoid so many imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import CustomerReviews from "./sections/CustomerReviews";
-import Footer from "./sections/Footer";
-import Hero from "./sections/Hero";
-import PopularProducts from "./sections/PopularProducts";
-import Services from "./sections/Services";
-import SpecialOffer from "./sections/SpecialOffer";
-import Subscribe from "./sections/Subscribe";
-import SuperQuality from "./sections/SuperQuality";
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections/index";
 
 // () immediately returns, so when function doesn't
 // have methods like console log we use () instead of {}.

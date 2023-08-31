3e38afc458fb83eb954bae68878b14c14b4ea0fb
JustVibhor <vibhorebhatneriya@gmail.com>
2023-08-31 15:48:17 +0530

    Importing Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-// () immediately returns, so when function doesn't have methods like console log we use () instead of {}.
+import CustomerReviews from "./sections/CustomerReviews";
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import Services from "./sections/Services";
+import SpecialOffer from "./sections/SpecialOffer";
+import Subscribe from "./sections/Subscribe";
+import SuperQuality from "./sections/SuperQuality";
+
+// () immediately returns, so when function doesn't
+// have methods like console log we use () instead of {}.
 const App = () => (
   <main className="relative">
     Nav
-    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-    <section className="padding">PopularProducts</section>
-    <section className="padding">SuperQuality</section>
-    <section className="padding-x">Services</section>
-    <section className="padding">SpecialOffer</section>
-    <section className="bg-pale-blue padding">CustomerReviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
   </main>
 );
 

5242ccbac86ce44d852c3067f4cbfc5533af7c52
JustVibhor <vibhorebhatneriya@gmail.com>
2023-09-05 11:36:06 +0530

    Adding navlinks

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
 
 const Nav = () => {
   // always use Semantic tags for better accessibility
@@ -17,8 +18,19 @@ const Nav = () => {
         </a>
         <ul
           className="flex-1 flex
-        justify-center items-center gap-16 max-lg:hidder"
-        ></ul>
+        justify-center items-center gap-16 max-lg:hidden"
+        >
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   );

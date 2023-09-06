34cf80c02522159337ea786d8353e4434758180f
JustVibhor <vibhorebhatneriya@gmail.com>
2023-09-06 15:09:26 +0530

    Change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,21 +16,26 @@ const Nav = () => {
         <a href="">
           <img src={headerLogo} alt="logo" width={130} height={29} />
         </a>
-        <ul
-          className="flex-1 flex
-        justify-center items-center gap-16 max-lg:hidden"
-        >
-          {navLinks.map((item) => (
-            <li key={item.label}>
-              <a
-                href={item.href}
-                className="font-montserrat leading-normal text-lg text-slate-gray"
-              >
-                {item.label}
-              </a>
-            </li>
-          ))}
-        </ul>
+        <div>
+          <ul
+            className="flex-1 flex
+          justify-center items-center gap-16 max-lg:hidden"
+          >
+            {navLinks.map((item) => (
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className="font-montserrat leading-normal text-lg text-slate-gray"
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className="hidden max-lg:block">
+            <img src={hamburger} alt={hamburger} width={25} height={25} />
+          </div>
+        </div>
       </nav>
     </header>
   );
